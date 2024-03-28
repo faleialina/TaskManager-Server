82d92ead7a4b1ff2b19e8e1ce3cccc7287aac1bb
a.falei <august3@mail.ru>
2024-03-28 23:17:08 +0300

    fix test

--- src/tests/controller/task.controller.test.ts
+++ src/tests/controller/task.controller.test.ts
@@ -2,11 +2,11 @@ import supertest from 'supertest'
 import { app } from '../../app'
 
 test('post/task', async () => {
-    const res = await supertest(app).post('/task').send({ task: 'js', user_id: 5 });
+    const res = await supertest(app).post('/task').send({ task: 'js', user_id: 6 });
 console.log(res.body);
 
     expect(res.statusCode).toBe(200);
     expect(res.body).toHaveLength(1);
     expect(res.body[0].task).toBe('js');
-    expect(res.body[0].user_id).toBe(5);
+    expect(res.body[0].user_id).toBe(6);
 })
